873f9fedf53342fd0c10e4e88f0ea1bde620bc1d
logan <lopar6@gmail.com>
2022-03-25 16:31:37 -0600

    move vpn widget to a different file

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -1,5 +1,6 @@
 import 'package:custom_panel/services/vpn.dart';
 import 'package:custom_panel/widgets/docker_widget.dart';
+import 'package:custom_panel/widgets/vpn_widget.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -13,30 +14,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Vpn vpn = Vpn();
-  VpnConnectionStatus vpnConnectionStatus = VpnConnectionStatus.changing;
   bool useDockerCompose = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _initAsync();
-  }
-
-  Future<void> _initAsync() async {
-    try {
-      if (await vpn.isConnected()) {
-        vpnConnectionStatus = VpnConnectionStatus.connected;
-      } else {
-        vpnConnectionStatus = VpnConnectionStatus.disconnected;
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print(e);
-      }
-    }
-    if (mounted) setState(() {});
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,53 +30,7 @@ class _HomePageState extends State<HomePage> {
           crossAxisCount: 3,
           shrinkWrap: true,
           children: [
-            Row(
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      vpnConnectionStatus.name.toString(),
-                    ),
-                    const SizedBox(height: 15),
-                    ElevatedButton(
-                      child: const Text("Connect"),
-                      onPressed: vpnConnectionStatus ==
-                              VpnConnectionStatus.disconnected
-                          ? () async {
-                              vpnConnectionStatus =
-                                  VpnConnectionStatus.changing;
-                              setState(() {});
-                              await vpn.connect();
-                              await vpn
-                                  .isConnected(VpnCheckStrategy.waitForConnect);
-                              vpnConnectionStatus =
-                                  VpnConnectionStatus.connected;
-                              setState(() {});
-                            }
-                          : null,
-                    ),
-                    const SizedBox(height: 15),
-                    ElevatedButton(
-                      child: const Text("Disconnect"),
-                      onPressed:
-                          vpnConnectionStatus == VpnConnectionStatus.connected
-                              ? () async {
-                                  vpnConnectionStatus =
-                                      VpnConnectionStatus.changing;
-                                  setState(() {});
-                                  await vpn.disconnect();
-                                  await vpn.isConnected(
-                                      VpnCheckStrategy.waitForDisconnect);
-                                  vpnConnectionStatus =
-                                      VpnConnectionStatus.disconnected;
-                                  setState(() {});
-                                }
-                              : null,
-                    ),
-                  ],
-                ),
-              ],
-            ),
+            const VpnWidget(),
             Column(
               children: [
                 const Text("Select A Project:", textScaleFactor: 2),
---
+++ lib/widgets/vpn_widget.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../services/vpn.dart';
+
+class VpnWidget extends StatefulWidget {
+  const VpnWidget({Key? key}) : super(key: key);
+
+  @override
+  State<VpnWidget> createState() => _VpnWidgetState();
+}
+
+class _VpnWidgetState extends State<VpnWidget> {
+  Vpn vpn = Vpn();
+  VpnConnectionStatus vpnConnectionStatus = VpnConnectionStatus.changing;
+
+  @override
+  void initState() {
+    super.initState();
+    _initAsync();
+  }
+
+  Future<void> _initAsync() async {
+    try {
+      if (await vpn.isConnected()) {
+        vpnConnectionStatus = VpnConnectionStatus.connected;
+      } else {
+        vpnConnectionStatus = VpnConnectionStatus.disconnected;
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+    if (mounted) setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          children: [
+            Text(
+              vpnConnectionStatus.name.toString(),
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              child: const Text("Connect"),
+              onPressed: vpnConnectionStatus == VpnConnectionStatus.disconnected
+                  ? () async {
+                      vpnConnectionStatus = VpnConnectionStatus.changing;
+                      setState(() {});
+                      await vpn.connect();
+                      await vpn.isConnected(VpnCheckStrategy.waitForConnect);
+                      vpnConnectionStatus = VpnConnectionStatus.connected;
+                      setState(() {});
+                    }
+                  : null,
+            ),
+            const SizedBox(height: 15),
+            ElevatedButton(
+              child: const Text("Disconnect"),
+              onPressed: vpnConnectionStatus == VpnConnectionStatus.connected
+                  ? () async {
+                      vpnConnectionStatus = VpnConnectionStatus.changing;
+                      setState(() {});
+                      await vpn.disconnect();
+                      await vpn.isConnected(VpnCheckStrategy.waitForDisconnect);
+                      vpnConnectionStatus = VpnConnectionStatus.disconnected;
+                      setState(() {});
+                    }
+                  : null,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
